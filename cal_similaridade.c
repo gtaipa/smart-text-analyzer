57ea85da34616d1099da3855ee461070f3764cfc
PedroRodrigues <pedrojoseabreurodrigues@gmail.com>
2025-11-25 20:56:46 +0000

    Ajustes em algumas funcoes mais confusas e comentarios de guia

--- cal_similaridade.c
+++ cal_similaridade.c
@@ -7,13 +7,11 @@
  */
 static double calcularMagnitude(int *vetor, int tamanhoVocabulario) {
     double soma_quadrados = 0.0;
-
-    // 1. Fazer a SOMA dos quadrados
+    // Fazer a SOMA dos quadrados
     for(int i=0; i<tamanhoVocabulario; i++) {
         soma_quadrados += (double)vetor[i] * (double)vetor[i];
     }
-
-    // 2. Calcular a raiz quadrada UMA VEZ, no fim.
+    //Calcular a raiz quadrada de acordo com a formula.
     return sqrt(soma_quadrados);
 }
 
@@ -21,7 +19,7 @@ static double calcularMagnitude(int *vetor, int tamanhoVocabulario) {
 //  Funcoes R1.6 (Publicas)
 // =============================================================
 
-double calcularProdutoInterno(int *vetorA, int *vetorB, int tamanhoVocabulario) {
+double calcularProdutoInterno(int *vetorA, int *vetorB, int tamanhoVocabulario) {//soma o produto dos conteudos presentes nas varias posicoes dos vetores
     double soma = 0.0;
     for (int i = 0; i < tamanhoVocabulario; i++) {
         soma += (double)vetorA[i] * (double)vetorB[i];
@@ -30,18 +28,13 @@ double calcularProdutoInterno(int *vetorA, int *vetorB, int tamanhoVocabulario) 
 }
 
 
-double calcularSimilaridadeCosseno(int *vetorA, int *vetorB, int tamanhoVocabulario) {
-    
+double calcularSimilaridadeCosseno(int *vetorA, int *vetorB, int tamanhoVocabulario) {//usamos as funcoes anteriores para simplificar o processo
     double produto_interno = calcularProdutoInterno(vetorA, vetorB, tamanhoVocabulario);
-    
-    // Chama a funcao auxiliar 'static'
     double magnitudeA = calcularMagnitude(vetorA, tamanhoVocabulario);
     double magnitudeB = calcularMagnitude(vetorB, tamanhoVocabulario);
-    
     // Evitar divisao por zero se um dos vetores for nulo
     if (magnitudeA == 0.0 || magnitudeB == 0.0) {
         return 0.0;
     }
-    
-    return produto_interno / (magnitudeA * magnitudeB);
+    return produto_interno / (magnitudeA * magnitudeB);//retornar o resultado que so faltava fazer a divisao
 }
